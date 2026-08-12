19e955b8954416ff4a508b4dffc7c80055fecb19
puspendra pandey <pandey48999@gmail.com>
2026-08-12 14:51:01 +0530

    add new feature salary calculate

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Employee = require("../models/employee");
+const Employee = require("../models/Employee");
 
 // ===============================
 // CREATE EMPLOYEE
